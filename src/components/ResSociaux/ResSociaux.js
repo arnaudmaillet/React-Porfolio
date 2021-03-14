0d91dbabe069f7e954ff0cc410c48b7d1fb92100
arnaudMaillet <arnaud.maillet@saint-remi.net>
2021-03-14 16:11:25 +0100

    14/03/2021 Update Reseaux_Sociaux

--- src/components/ResSociaux/ResSociaux.js
+++ src/components/ResSociaux/ResSociaux.js
@@ -1,23 +1,16 @@
 import React from "react";
-import Style from "./ResSociaux.module.scss"
+import Style from "./ResSociaux.module.scss";
+import ResSocial from './ResSocial/ResSocial.js'
+
 
 const ResSociaux = () => {
-
      return(
         <div className={Style.reseau_sociaux}>
             <div>
-                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-                    <div><i className="fab fa-facebook-f"></i></div>
-                </a>
-                <a href="https://twitter.com/" target="_blank" rel="noreferrer">
-                    <div><i className="fab fa-twitter"></i></div>
-                </a>
-                <a href="https://fr.linkedin.com/" target="_blank" rel="noreferrer">
-                    <div><i className="fab fa-linkedin"></i></div>
-                </a>
-                <a href="https://github.com/arnaudmaillet" target="_blank" rel="noreferrer">
-                    <div><i className="fab fa-github"></i></div>
-                </a>
+                <ResSocial lien="https://www.facebook.com/" icone="fab fa-facebook-f">Facebook</ResSocial>
+                <ResSocial lien="https://twitter.com/" icone="fab fa-twitter">Twitter</ResSocial>
+                <ResSocial lien="https://fr.linkedin.com/" icone="fab fa-linkedin">Linkedin</ResSocial>
+                <ResSocial lien="https://github.com/arnaudmaillet" icone="fab fa-github">GitHub</ResSocial>
             </div>
         </div>
     );
