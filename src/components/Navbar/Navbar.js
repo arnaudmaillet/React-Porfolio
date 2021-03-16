d3df931cd151e019863e047c6099b2eb70da9076
arnaudMaillet <arnaud.maillet@saint-remi.net>
2021-03-16 23:57:26 +0100

    16/03/2021 Add DropdownMenu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react"
-import { NavLink } from 'react-router-dom';
-import Style from './Navbar.module.scss'
+import { CSSTransition } from 'react-transition-group'
+// import { NavLink } from 'react-router-dom';
+import './Navbar.scss'
+import './Navbar.css'
+
 import { ReactComponent as DarkMod} from '../../assets/svg/adjust-solid.svg'
 import { ReactComponent as Home} from '../../assets/svg/home-solid.svg'
 import { ReactComponent as Profil} from '../../assets/svg/user-circle-regular.svg'
 import { ReactComponent as Skill} from '../../assets/svg/graduation-cap-solid.svg'
 import { ReactComponent as Work} from '../../assets/svg/laptop-code-solid.svg'
 import { ReactComponent as Other} from '../../assets/svg/bitcoin-brands.svg'
+import { ReactComponent as LeftArrow} from '../../assets/svg/chevron-left-solid.svg'
+import { ReactComponent as RightArrow} from '../../assets/svg/long-arrow-alt-right-solid.svg'
 
 const Navbar = () => {
 
      return(
-        <div className={Style.navbar}>
-            <ul className={Style.navbar_nav}>
+        <div className="navbar">
+            <ul className="navbar_nav">
                 <NavItems content="Menu">
                     <DropdownMenu></DropdownMenu>
                 </NavItems>
@@ -28,8 +33,8 @@ const NavItems = (props) =>{
     const [open, setOpen] = useState(false);
 
     return(
-        <div className={Style.nav_item}>    
-            <li className={Style.item_content} onClick={() => setOpen(!open)}>
+        <div className="nav_item">    
+            <li className="item_content" onClick={() => setOpen(!open)}>
                 {props.content}
             </li>
             {open && props.children}
@@ -39,42 +44,81 @@ const NavItems = (props) =>{
 
 const DropdownMenu = () =>{
 
+    const [activeMenu, setActiveMenu] =  useState('main');
+    const [menuHeight, setMenuHeight] = useState(null);
+
+    function calcHeight(el) {
+        const height = el.offsetHeight + 30;
+        setMenuHeight(height);
+      }
+
     const DropdownItem = (props) => {
         return(
-            <div className={Style.menu_item}>
-                <span className={Style.icon_left}>{props.leftIcon}</span>
+            <div className="menu_item" onClick={()=> props.goToMenu && setActiveMenu(props.goToMenu)}>
+                <span className="icon_left">{props.leftIcon}</span>
                 {props.children}
-                <span className={Style.right}>{props.rightIcon}</span>
+                <span className="right">{props.rightIcon}</span>
             </div>
         )
     }
     return(
-        <div className={Style.dropdown}>
-            <NavLink to='/' exact style={{textDecoration: 'none'}}>
-                <DropdownItem leftIcon={<Home/>}>
-                    Accueil
-                </DropdownItem>
-            </NavLink>
-            <NavLink to='/profil' exact style={{textDecoration: 'none'}}>
-                <DropdownItem leftIcon={<Profil/>}>
-                    Mon Profil
-                </DropdownItem>
-            </NavLink>
-            <NavLink to='/formations' exact style={{textDecoration: 'none'}}>
-                <DropdownItem leftIcon={<Skill/>}>
-                    Formations
-                </DropdownItem>
-            </NavLink>
-            <NavLink to='/projets' exact style={{textDecoration: 'none'}}>
-                <DropdownItem leftIcon={<Work/>}>
-                    Projets
-                </DropdownItem>
-            </NavLink>
-            <NavLink to='/autres' exact style={{textDecoration: 'none'}}>
-                <DropdownItem leftIcon={<Other/>}>
-                    Autres
-                </DropdownItem>
-            </NavLink>
+        <div className="dropdown" style={{ height: menuHeight }}>
+            <CSSTransition    
+                in={activeMenu === 'main'} 
+                unmountOnExit
+                timeout={500}
+                classNames="menu-primary"
+                onEnter={calcHeight}
+                >
+                <div className="menu">
+                    <DropdownItem leftIcon={<Home/>}>
+                        Accueil
+                    </DropdownItem>
+                    <DropdownItem 
+                        leftIcon={<Profil/>}
+                        goToMenu="accueil"
+                        >
+                        Mon Profil
+                    </DropdownItem>
+                    <DropdownItem leftIcon={<Skill/>}>
+                        Formations
+                    </DropdownItem>                  
+                    <DropdownItem leftIcon={<Work/>}>
+                        Projets
+                    </DropdownItem>
+                    <DropdownItem leftIcon={<Other/>}>
+                        Autres
+                    </DropdownItem>
+                </div>
+            </CSSTransition>
+
+            <CSSTransition    
+                in={activeMenu === 'accueil'} 
+                unmountOnExit 
+                timeout={500}
+                classNames="menu-secondary"
+                onEnter={calcHeight}
+                >
+                <div className="menu">
+                    <DropdownItem 
+                        leftIcon={<LeftArrow/>}
+                        goToMenu="main"
+                        >
+                    </DropdownItem>
+                    <DropdownItem 
+                        leftIcon={<RightArrow/>}
+                        >Profil
+                    </DropdownItem>
+                    <DropdownItem 
+                        leftIcon={<RightArrow/>}
+                        >CV
+                    </DropdownItem>
+                    <DropdownItem 
+                        leftIcon={<RightArrow/>}
+                        >Parcours
+                    </DropdownItem>
+                </div>
+            </CSSTransition>
         </div>
     )
 }
